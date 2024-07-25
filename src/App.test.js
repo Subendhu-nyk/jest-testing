1a0ee3872eab0a818d1e93746b0b207b717da0bf
Subendhu-nyk <subendhu.nyk@gmail.com>
2024-07-25 19:48:02 +0530

    simple test case

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // const linkElement = screen.getByText(/learn react/i);
+  // getByText- it will search for test learn reactin App.js if found test passes otherwise failed
+  const linkElement = screen.getByText(/sam/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test takes 2 arguments
+// 1. description of the test.
+// 2.Anonymous function
+w
